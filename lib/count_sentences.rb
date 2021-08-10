0a97a706902ae85e4d976588c3c55943f1d99389
Quinn Lonergan <qui99lo@gmail.com>
2021-08-10 17:43:57 -0400

    commit tree 8953477ed191349cd7c7b7d0a1bf343e52282570

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else return false
+    end
   end
 
   def count_sentences
-
+    sentences = self.split(/[.!?]/)
+    new_sentences = sentences.filter do |sentence|
+      sentence != ""
+    end
+    new_sentences.count
   end
 end
